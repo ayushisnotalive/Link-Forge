7487ab7d3b008b5ea1788dceea7025a99eca727b
ayushisnotalive <theayushchakraborty@gmail.com>
2026-09-13 12:28:51 +0530

    test-fix

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -23,7 +23,7 @@ export default function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-            <Route path="*" element={<Navigate to="/dashboard" replace />} />
+            <Route path="/" element={<Navigate to="/login" replace />} />
           </Routes>
         </BrowserRouter>
       </AuthProvider>
