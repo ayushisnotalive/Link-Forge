b5e0ea64446eb18d103fd49b9543fc067fa3bff4
ayushisnotalive <theayushchakraborty@gmail.com>
2026-09-23 21:26:00 +0530

    fix: remove test scripts with exposed credentials

--- packages/api/test-pg-no-ssl.js
+++
@@ -1,3 +0,0 @@
-import pg from 'pg';
-const pool = new pg.Pool({ connectionString: 'postgresql://neondb_owner:npg_I4rvWLqjaGU2@ep-little-bread-ae6hm5ta-pooler.c-2.us-east-2.aws.neon.tech/neondb' });
-pool.query('SELECT 1').then(res => { console.log('Connected!'); process.exit(0); }).catch(err => { console.error('Error:', err.message); process.exit(1); });
